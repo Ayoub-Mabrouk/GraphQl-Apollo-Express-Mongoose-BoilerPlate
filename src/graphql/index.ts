f46e17c9ff1d95e037547038c23baf96bb0413df
Ayoub Mabrouk <77799760+Ayoub-Mabrouk@users.noreply.github.com>
2022-04-07 01:43:19 +0000

    adding default schemas/resolversLoaders/typedefsLoaders

---
+++ src/graphql/index.ts
@@ -0,0 +1,14 @@
+import { join } from 'path';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import {  mergeResolvers } from '@graphql-tools/merge';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+// Load all resolvers
+const resolversPath = join(__dirname, './**/resolver.*');
+const resolversArray = loadFilesSync(resolversPath);
+export const resolvers = mergeResolvers(resolversArray);
+
+//Load all typeDefs
+const typeDefsPath = join(__dirname, './**/schema.graphql');
+const typeDefsArray = loadFilesSync(typeDefsPath);
+export const typeDefs =  mergeTypeDefs(typeDefsArray);
