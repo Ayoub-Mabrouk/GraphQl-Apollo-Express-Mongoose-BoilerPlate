c1379f8d3ac9c98223e321658c1714499b6be074
Ayoub Mabrouk <77799760+Ayoub-Mabrouk@users.noreply.github.com>
2022-04-12 22:53:42 +0000

    adding cach params to the rules

--- src/permissions/rules.ts
+++ src/permissions/rules.ts
@@ -1,22 +1,25 @@
 import { rule } from 'graphql-shield';
 
-export const isUser = rule()(async (parent, args, { user }, info) => {
+// cache:contextual - use when rule only relies on context parameter (useful for authentication).
+export const isUser = rule({ cache: 'contextual' })(async (parent, args, { user }, info) => {
     try {
 
         if (user && user.roles.includes("user")) {
             return true;
         }
-        return false;
+        // Shield, by default, catches all errors thrown during resolver execution
+        // To return custom error messages to your client, you can return error instead of throwing it
+        return  new Error("Nice try,You can't do that 😕");
     } catch (err) {
         throw err;
     }
 });
-export const isAdmin = rule()(async (parent, args, { user }, info) => {
+export const isAdmin = rule({ cache: 'contextual' })(async (parent, args, { user }, info) => {
     try {        
         if (user && user.roles.includes("admin")) {
             return true;
         }
-        return false;
+        return  new Error("Nice try,You can't do that 😕");
     } catch (err) {
         throw err;
     }
