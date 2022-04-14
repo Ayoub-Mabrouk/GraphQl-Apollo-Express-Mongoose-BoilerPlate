719d801e2d042bb8d46196cd269cebdce20202c2
Ayoub Mabrouk <77799760+Ayoub-Mabrouk@users.noreply.github.com>
2022-04-14 09:52:00 +0000

    add deny to permissions

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -1,7 +1,9 @@
-import { shield, or } from 'graphql-shield';
+import { shield, or, deny } from 'graphql-shield';
 import {isAdmin, isUser} from './rules';
 export const permissions = shield({
     Query: {
+        // deny access to all queries that doent have a rule
+      "*":deny,
       users: isAdmin,
       companies: or(isAdmin, isUser),
     },
